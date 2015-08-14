9a6a3099c0182503bc91f6e32b8e3a9a35e23cc6
rajansindkar <rajsindkar@outlook.com>
2015-08-13 22:34:00 -0400

    MVVM
    MVVM

---
+++ Model/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWpfApplication.Model
+{
+    class Customers
+    {
+        public int CustID { get; set; }
+        public string CustName { get; set; }
+    }
+}
---
+++ ViewModel/MyViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleWpfApplication.Model;
+namespace SampleWpfApplication.ViewModel
+{
+    class MyViewModel
+    {
+        Customers objCust = new Customers();
+        int i;
+        MyViewModel(string name) 
+        {
+            objCust.CustID = i;
+            objCust.CustName = name;
+            i++;
+        }
+        public Customers GetCustomers()
+        {
+            return objCust;
+        }
+    }
+}
